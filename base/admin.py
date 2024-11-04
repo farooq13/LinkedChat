519701db5a0f9287b008690b6eaa4fdba5e46b8e
farooq13 <farukidris19@gmail.com>
2024-11-04 16:45:42 +0100

    add notification

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Post, Comment, UserProfile
+from .models import Post, Comment, UserProfile, Notification
 
 
 
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(UserProfile)
+admin.site.register(Notification)
